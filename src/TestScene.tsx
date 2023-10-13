4e87f9929b4ddcdac8e3aef8aa4ce0cbe48513b7
Charles Kamiri <charleskamiri6@gmail.com>
2023-10-13 17:04:11 +0300

    Added surface

--- src/TestScene.tsx
+++ src/TestScene.tsx
@@ -9,12 +9,30 @@ const TestScene = () => {
         const camera = new Three.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
         const renderer = new Three.WebGLRenderer();
 
-        const geometry = new Three.BoxGeometry(1, 1, 1);
-        const material = new Three.MeshBasicMaterial( {color: 0x00ff00} );
-        const cube = new Three.Mesh(geometry, material);
-        scene.add(cube);
-
         camera.position.z = 5;
+
+
+        // Light
+        const light = new Three.DirectionalLight(0xffffff, 1);
+        light.position.y = 3;
+        light.position.z = 2;
+
+        const cube = new Three.Mesh(
+          new Three.BoxGeometry(1, 1, 1),
+          new Three.MeshStandardMaterial({ color: 0x00ff00 })
+        );
+
+        const surface = new Three.Mesh(
+            new Three.BoxGeometry(5, 0.5, 10),
+            new Three.MeshStandardMaterial( {color: 0x0000ff})
+        );
+        surface.position.y = -2;
+
+
+        scene.add(cube);
+        scene.add(surface);
+        scene.add(light);
+
 
         renderer.setSize(window.innerWidth, window.innerHeight);
 
